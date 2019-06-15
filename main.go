52dd6325368ab87b2e4802fa3ea6c8f20401be87
Ivan Emiliano Noriega <ivanenoriega@gmail.com>
2019-06-15 17:07:11 -0300

    class 77 - Repeating goroutines

--- main.go
+++ main.go
@@ -14,17 +14,25 @@ func main() {
 		"https://udemy.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, error := http.Get(link)
 	if error != nil {
 		fmt.Println(link, "might be down")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is ok")
+	c <- link
 }
